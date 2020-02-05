7c8e017a9ccf4641886f606a9b7ff99983bfdba0
Juan Marcelo Cuevas <marce.cuevas16@gmail.com>
2020-02-05 16:19:59 -0300

    Initial Commit

---
+++ SwiftUITest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITest
+//
+//  Created by Juan Marcelo Cuevas on 2/5/20.
+//  Copyright © 2020 Juan Marcelo Cuevas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
